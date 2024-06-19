d76a4e6eff35cf003d485b26b10f273ba803c183
mini7007 <mohan5050390@gmail.com>
2024-06-19 12:27:18 +0530

    new changes

--- Create Hello World Function.js
+++ Create Hello World Function.js
@@ -1,11 +1,5 @@
-/**
- * @return {Function}
- */
-var createHelloWorld = function () {
-  return function (...args) {};
-};
-
-/**
- * const f = createHelloWorld();
- * f(); // "Hello World"
- */
+function createHelloWorld() {
+  return function () {
+    return "Hello World";
+  };
+}
