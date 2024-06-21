c55a803c568164fec3dee2a2ff6ce569706a30a0
mini7007 <mohan5050390@gmail.com>
2024-06-21 13:00:03 +0530

    new updated files

--- To Be Or Not To Be.js
+++ To Be Or Not To Be.js
@@ -1,10 +1,17 @@
-/**
- * @param {string} val
- * @return {Object}
- */
-var expect = function (val) {};
-
-/**
- * expect(5).toBe(5); // true
- * expect(5).notToBe(5); // throws "Equal"
- */
+function expect(val) {
+    return {
+      toBe: function (val2) {
+        if (val === val2) {
+          return true;
+        }
+        throw 'Not Equal';
+      },
+      notToBe: function (val2) {
+        if (val !== val2) {
+          return true;
+        }
+        throw 'Equal';
+      },
+    };
+  }
+  
