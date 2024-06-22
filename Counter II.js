750f4797d77c2128511cfa495fef01028f363afe
mini7007 <mohan5050390@gmail.com>
2024-06-22 18:09:42 +0530

    new code updated

--- Counter II.js
+++ Counter II.js
@@ -1,12 +1,9 @@
-/**
- * @param {integer} init
- * @return { increment: Function, decrement: Function, reset: Function }
- */
-var createCounter = function (init) {};
-
-/**
- * const counter = createCounter(5)
- * counter.increment(); // 6
- * counter.reset(); // 5
- * counter.decrement(); // 4
- */
+function createCounter(init) {
+    let cur = init;
+    return {
+      increment: () => ++cur,
+      decrement: () => --cur,
+      reset: () => (cur = init),
+    };
+  }
+  
