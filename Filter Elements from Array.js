a3ec857a2645fa7dc1423132b8eae047af745ccc
mini7007 <mohan5050390@gmail.com>
2024-06-24 12:02:22 +0530

    updated code

--- Filter Elements from Array.js
+++ Filter Elements from Array.js
@@ -1,6 +1,19 @@
 /**
- * @param {number[]} arr
- * @param {Function} fn
- * @return {number[]}
+ * @typedef {(n: number, i: number) => any} Fn
  */
-var filter = function (arr, fn) {};
+
+/**
+ * Filters an array based on a provided function.
+ * @param {number[]} arr - The array to filter.
+ * @param {Fn} fn - The function used to test each element.
+ * @returns {number[]} - The filtered array.
+ */
+function filter(arr, fn) {
+  const ans = [];
+  arr.forEach((a, index) => {
+    if (fn(a, index)) {
+      ans.push(a);
+    }
+  });
+  return ans;
+}
