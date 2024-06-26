8d9afaf18a30eacaf017aec08650ff12b1d858b4
mini7007 <mohan5050390@gmail.com>
2024-06-26 10:58:22 +0530

    updated code

--- Function Composition.js
+++ Function Composition.js
@@ -1,12 +1,14 @@
 /**
- * @param {Function[]} functions
- * @return {Function}
+ * @typedef {function(number): number} F
  */
-var compose = function (functions) {
-  return function (x) {};
-};
 
 /**
- * const fn = compose([x => x + 1, x => 2 * x])
- * fn(4) // 9
+ * Composes an array of functions from right to left.
+ * @param {F[]} functions - An array of functions to compose.
+ * @returns {F} - A function that is the composition of the input functions.
  */
+function compose(functions) {
+  return function (x) {
+    return functions.reduceRight((val, f) => f(val), x);
+  };
+}
