c753da11c7b24ba091118c6742a9c778934c6ec6
mini7007 <mohan5050390@gmail.com>
2024-07-01 11:29:55 +0530

    updated code

--- Add Two Promises.js
+++ Add Two Promises.js
@@ -1,11 +1,4 @@
-/**
- * @param {Promise} promise1
- * @param {Promise} promise2
- * @return {Promise}
- */
-var addTwoPromises = async function (promise1, promise2) {};
-
-/**
- * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
- *   .then(console.log); // 4
- */
+async function addTwoPromises(promise1, promise2) {
+  const results = await Promise.all([promise1, promise2]);
+  return results[0] + results[1];
+}
