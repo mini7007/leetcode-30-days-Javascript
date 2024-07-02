b666efa0c056a1b2f4d31971f8abb56a17cdf61f
mini7007 <mohan5050390@gmail.com>
2024-07-02 10:44:47 +0530

    updated code

--- Sleep.js
+++ Sleep.js
@@ -1,10 +1,5 @@
-/**
- * @param {number} millis
- * @return {Promise}
- */
-async function sleep(millis) {}
-
-/**
- * let t = Date.now()
- * sleep(100).then(() => console.log(Date.now() - t)) // 100
- */
+async function sleep(millis) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, millis);
+  });
+}
