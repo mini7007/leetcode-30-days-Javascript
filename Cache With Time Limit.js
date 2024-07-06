557eda05826ee9ca50a06731ad0d3df1578305db
mini7007 <mohan5050390@gmail.com>
2024-07-06 11:17:24 +0530

    updated code

--- Cache With Time Limit.js
+++ Cache With Time Limit.js
@@ -1,42 +1,32 @@
-var TimeLimitedCache = function () {
-  this.cache = new Map();
-};
-
-/**
- * @param {number} key
- * @param {number} value
- * @param {number} time until expiration in ms
- * @return {boolean} if un-expired key already existed
- */
-TimeLimitedCache.prototype.set = function (key, value, duration) {
-  const valueInCache = this.cache.get(key);
-  if (valueInCache) {
-    clearTimeout(valueInCache.timeout);
+class TimeLimitedCache {
+  constructor() {
+    this.cache = new Map();
   }
-  const timeout = setTimeout(() => this.cache.delete(key), duration);
-  this.cache.set(key, { value, timeout });
-  return Boolean(valueInCache);
-};
 
-/**
- * @param {number} key
- * @return {number} value associated with key
- */
-TimeLimitedCache.prototype.get = function (key) {
-  return this.cache.has(key) ? this.cache.get(key).value : -1;
-};
+  set(key, value, duration) {
+    const now = Date.now();
+    const exists = this.getValue(now, key) !== undefined;
+    this.cache.set(key, { value: value, expiredAt: now + duration });
+    return exists;
+  }
 
-/**
- * @return {number} count of non-expired keys
- */
-TimeLimitedCache.prototype.count = function () {
-  return this.cache.size;
-};
+  get(key) {
+    const val = this.getValue(Date.now(), key);
+    return val === undefined ? -1 : val.value;
+  }
 
-/**
- * Your TimeLimitedCache object will be instantiated and called as such:
- * var obj = new TimeLimitedCache()
- * obj.set(1, 42, 1000); // false
- * obj.get(1) // 42
- * obj.count() // 1
- */
+  count() {
+    const now = Date.now();
+    for (const key of this.cache.keys()) {
+      if (this.getValue(now, key) === undefined) {
+        this.cache.delete(key);
+      }
+    }
+    return this.cache.size;
+  }
+
+  getValue(now, key) {
+    const val = this.cache.get(key);
+    return val && now <= val.expiredAt ? val : undefined;
+  }
+}
