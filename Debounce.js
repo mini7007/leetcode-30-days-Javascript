54b21575998eed6dd6f16fa3a206e693eed132c4
mini7007 <mohan5050390@gmail.com>
2024-07-07 12:51:43 +0530

    updated code

--- Debounce.js
+++ Debounce.js
@@ -1,18 +1,7 @@
-/**
- * @param {Function} fn
- * @param {number} t milliseconds
- * @return {Function}
- */
-var debounce = function (fn, t) {
-  let id;
+function debounce(fn, t) {
+  let timeout;
   return function (...args) {
-    clearTimeout(id);
-    id = setTimeout(() => fn(...args), t);
+    clearTimeout(timeout);
+    timeout = setTimeout(() => fn(...args), t);
   };
-};
-/**
- * const log = debounce(console.log, 100);
- * log('Hello'); // cancelled
- * log('Hello'); // cancelled
- * log('Hello'); // Logged at t=100ms
- */
+}
